e536b93e72b5a170f777fd25b311ab0f22ebe8d9
Tuca-DEV <guga_camil@hotmail.com>
2023-01-04 17:49:43 -0300

    Alterações e correções na função de mutação

--- mutacao.m
+++ mutacao.m
@@ -1,12 +1,12 @@
-function [populacaoMutada] = mutacao(m)
+function [filhosMutados] = mutacao(m)
 
   probabilidade = 0;
-  populacaoMutada = m;
+  filhosMutados = m;
   for i = 1:2
     for  j = 1:6
       probabilidade = rand();
       if(probabilidade >= 0.95)
-         populacaoMutada(i,j) = 1 - m(i,j);
+         filhosMutados(i,j) = 1 - m(i,j);
       endif
     endfor
   endfor
