9dd2a9018eee0b7c7156ad9538468b38e7281854
Livia Caetano de Andrade <liviacaetanodeandrade@gmail.com>
2023-01-05 11:01:49 -0400

    corrige nome da variável

--- main.m
+++ main.m
@@ -1,26 +1,26 @@
 disp('Primeira população/geração')
-populacao(1:10, 1:6) = populacao();
-populacao(11:12, 1:6) = -1
+populacaoInit = populacao();
+populacaoInit(11:12, 1:6) = -1
 melhor_da_geracao = zeros(1,10);
-populacaoDec = convertBinToDec(populacao, 12)
+populacaoDec = convertBinToDec(populacaoInit, 12)
 melhor_da_geracao(1) = max(populacaoDec);
 
 for i=2:10
   paisAptos = elitismo(populacaoDec);
-  filhos = crossOver(paisAptos,populacao);
+  filhos = crossOver(paisAptos,populacaoInit);
   filhosMutados = mutacao(filhos);
   espacos = avaliacao(populacaoDec);
-  populacao(espacos(1),:) = filhosMutados(1,:);
-  populacao(espacos(2),:) = filhosMutados(2,:);
+  populacaoInit(espacos(1),:) = filhosMutados(1,:);
+  populacaoInit(espacos(2),:) = filhosMutados(2,:);
   disp('População com os novos filhos')
-  populacao
-  populacaoDec = convertBinToDec(populacao, 12)
+  populacaoInit
+  populacaoDec = convertBinToDec(populacaoInit, 12)
   seresFracos = menosAptos(populacaoDec);
-  populacao(seresFracos(1), :) = -1;
-  populacao(seresFracos(2), :) = -1;
+  populacaoInit(seresFracos(1), :) = -1;
+  populacaoInit(seresFracos(2), :) = -1;
   disp('População depois de eliminar os fracos')
-  populacao
-  populacaoDec = convertBinToDec(populacao, 12)
+  populacaoInit
+  populacaoDec = convertBinToDec(populacaoInit, 12)
   melhor_da_geracao(i) = max(populacaoDec);
 endfor
 
