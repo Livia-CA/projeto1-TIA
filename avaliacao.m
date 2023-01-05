f4a8b783b44a9088a64aa91fef794a6c6c036357
Tuca-DEV <guga_camil@hotmail.com>
2023-01-04 22:22:59 -0300

    Finalização do programa main, da função avaliacao e adaptações na função menos Aptos

--- avaliacao.m
+++ avaliacao.m
@@ -1,8 +1,21 @@
-function avaliacao(filhos)
-  filhosSelecionados = zeros(1,10)
-  for i=1:10
-    if(filho(i) >= 45)
-      filhosSelecionados(i) = filho(i);
+function [espacos] = avaliacao(populacaoDec)
+  espacos = [0,0]; %Endeço/índice dos espaços nulos/vazios na matriz da população
+  c = 0; %Quantidade de espaços vazios encontrados
+  i = 1; %Índice para percorrer o vetor
+
+  %Enquanto não encontrar espaços vazios e o vetor da população em decimal não tiver acabado, faça:
+  while(c < 2 && i < 13)
+    if(populacaoDec(i) < 0)
+      c++;
+      espacos(c) = i;
     endif
-  endfor
+    i++;
+
+    %Para verificar erros de lógica
+    if(i > 13)
+    i
+    disp('Erro na avaliação')
+    endif
+
+  endwhile
 endfunction
--- menosAptos.m
+++ menosAptos.m
@@ -2,6 +2,11 @@ function [doismenosAptos] = menosAptos(populacao)
   doismenosAptos = zeros(1,2);
   for i= 1:2
     [valor, indice] = min(populacao);
+    if(valor < 0)
+      i--;
+      populacao(indice) = 100;
+      continue;
+    endif
     doismenosAptos(i) = indice;
     populacao(indice) = 100;
   endfor
